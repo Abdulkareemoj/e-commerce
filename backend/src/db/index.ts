70465fa692478a3a50e84a049aafb8c48c57de0d
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-12-08 11:19:01 +0000

    env stuff

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,10 +1,15 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import * as authSchema from './schema/auth-schema';
-import * as profileSchema from './schema/profile-schema';
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+import { drizzle } from "drizzle-orm/node-postgres";
+import * as authSchema from "./schema/auth-schema";
+import * as profileSchema from "./schema/profile-schema";
+import * as vendorSchema from "./schema/vendor-schema";
+import * as productSchema from "./schema/product-schema";
+
 export const schema = {
-	...authSchema,
-	...profileSchema,
-	
+  ...authSchema,
+  ...profileSchema,
+  ...vendorSchema,
+  ...productSchema,
 };
 export const db = drizzle(process.env.DATABASE_URL!, { schema });
