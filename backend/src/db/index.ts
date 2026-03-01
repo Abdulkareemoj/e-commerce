55c7daae50945abcf9f4222bbd9a5ee535485d46
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-03-01 12:34:49 +0100

    db stuff

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,15 +1,25 @@
 import dotenv from "dotenv";
-dotenv.config({ path: ".env" });
-import { drizzle } from "drizzle-orm/node-postgres";
+
+dotenv.config({ path: "../../.env.local" });
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
 import * as authSchema from "./schema/auth-schema";
 import * as profileSchema from "./schema/profile-schema";
 import * as vendorSchema from "./schema/vendor-schema";
 import * as productSchema from "./schema/product-schema";
+import * as orderSchema from "./schema/order-schema";
+import * as categorySchema from "./schema/category-schema";
+import * as addressSchema from "./schema/address-schema";
 
 export const schema = {
   ...authSchema,
   ...profileSchema,
   ...vendorSchema,
   ...productSchema,
+  ...orderSchema,
+  ...categorySchema,
+  ...addressSchema,
 };
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+const queryClient = postgres(process.env.DATABASE_URL!);
+export const db = drizzle({ client: queryClient }, { schema });
