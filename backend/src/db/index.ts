6e2c25573ee7893489b5a217a37053e898f36e0b
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-05-27 00:44:19 +0100

    feat:add env location and update schema

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config({ path: "../../.env.local" });
+const __dirname = dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: `${__dirname}/../../.env.local` });
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
@@ -8,18 +11,32 @@ import * as authSchema from "./schema/auth-schema";
 import * as profileSchema from "./schema/profile-schema";
 import * as vendorSchema from "./schema/vendor-schema";
 import * as productSchema from "./schema/product-schema";
+import * as productVariantSchema from "./schema/product-variant-schema";
 import * as orderSchema from "./schema/order-schema";
+import * as cartSchema from "./schema/cart-schema";
 import * as categorySchema from "./schema/category-schema";
 import * as addressSchema from "./schema/address-schema";
+import * as reviewSchema from "./schema/review-schema";
+import * as couponSchema from "./schema/coupon-schema";
+import * as wishlistSchema from "./schema/wishlist-schema";
+import * as messageSchema from "./schema/message-schema";
+import * as payoutSchema from "./schema/payout-schema";
 
 export const schema = {
   ...authSchema,
   ...profileSchema,
   ...vendorSchema,
   ...productSchema,
+  ...productVariantSchema,
   ...orderSchema,
+  ...cartSchema,
   ...categorySchema,
   ...addressSchema,
+  ...reviewSchema,
+  ...couponSchema,
+  ...wishlistSchema,
+  ...messageSchema,
+  ...payoutSchema,
 };
 const queryClient = postgres(process.env.DATABASE_URL!);
 export const db = drizzle({ client: queryClient }, { schema });
